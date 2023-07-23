28476391644627c1696313a7fe58b67149555fd4
João Prado <58610872+joaoprado96@users.noreply.github.com>
2023-07-22 22:27:01 -0300

    Atualização

--- front/analise.py
+++ front/analise.py
@@ -12,6 +12,13 @@ mydb = mysql.connector.connect(
   database="coderunner"
 )
 
+# Especificando o caminho do diretório
+directory = "C:/Users/joaop/OneDrive/Documents/GitHub/code-runner/public/"
+
+# Nome do arquivo
+filename = "version.js"
+filename2 = "id.js"
+
 mycursor = mydb.cursor()
 
 # SQL para selecionar os dados
@@ -51,49 +58,39 @@ for name, group in grouped_data:
     # Calcula a quantidade de id_teste únicos
     group_dict['quantidade_id_teste_unicos'] = group['id_teste'].nunique()
 
-
     # Adiciona o grupo ao JSON
     json_data[str(name)] = group_dict
-
-# # Agrupa os dados apenas por versao_grbe
-# grouped_data_version = data.groupby(['versao_grbe'])
-
-# for name, group in grouped_data_version:
-#     # Cria um dicionário para o grupo
-#     group_dict = {}
-#     # Calcula a quantidade de id_testes
-#     group_dict['quantidade_id_testes'] = int(group['id_teste'].nunique())
-#     # Calcula o tempo médio de execução de cada id_teste
-#     mean_exec_time = group.groupby('id_teste')['tempo_execucao'].mean().mean()
-#     group_dict['tempo_medio_execucao_id_teste'] = mean_exec_time if np.isnan(mean_exec_time) else None
-#     # Calcula o tempo total de execução
-#     group_dict['tempo_total_execucao'] = group['tempo_execucao'].sum()
-#     # Calcula a quantidade total de execuções
-#     group_dict['quantidade_total_execucoes'] = int(group['id_teste'].count())
-#     # Calcula a quantidade de executores diferentes
-#     group_dict['quantidade_executores'] = int(group['executor'].nunique())
-#     # Calcula a quantidade total de falhas
-#     group_dict['quantidade_total_falhas'] = int(group[group['status_teste'] == 'Falha']['id_teste'].count())
-#     # Calcula a média de falhas por id_teste
-#     mean_failures = group[group['status_teste'] == 'Falha']['id_teste'].nunique() / group['id_teste'].nunique()
-#     group_dict['media_falhas_por_id_teste'] = mean_failures if np.isnan(mean_failures) else None
-#     # Calcula a quantidade de testes por pilar
-#     group_dict['quantidade_testes_por_pilar'] = group.groupby('pilar')['id_teste'].count().to_dict()
-
-#     # Adiciona o grupo ao JSON
-#     json_data[str(name)] = group_dict
-
-# Imprime o JSON
-print(json.dumps(json_data, indent=4))
-
-# Especificando o caminho do diretório
-directory = "C:/Users/joaop/OneDrive/Documents/GitHub/code-runner/public/"
-
-# Nome do arquivo
-filename = "data.js"
 
 # Combina o diretório com o nome do arquivo
 full_path = os.path.join(directory, filename)
 
 with open(full_path, 'w') as file:
     file.write('let data = ' + json.dumps(json_data, indent=4) + ';')
+
+
+# Agrupa os dados por id_teste e versao_grbe
+grouped_data = data.groupby(['id_teste', 'versao_grbe'])
+
+# Cria o dicionário para o JSON
+json_data = {}
+
+for (id_teste, versao), group in grouped_data:
+    # Cria um dicionário para o grupo
+    group_dict = {}
+    # Calcula o tempo médio de execução
+    group_dict['tempo_medio_execucao'] = group['tempo_execucao'].mean()
+    # Calcula a quantidade de execuções
+    group_dict['quantidade_execucoes'] = int(group['id_teste'].count())
+    # Calcula a quantidade de executores
+    group_dict['quantidade_executores'] = int(group['executor'].nunique())
+    # Calcula a quantidade de falhas nos testes
+    group_dict['quantidade_falhas_teste'] = int(group[group['status_teste'] == 'Falha']['id_teste'].count())
+
+    # Adiciona o grupo ao JSON
+    json_data[str((id_teste, versao))] = group_dict
+
+# Combina o diretório com o nome do arquivo
+full_path = os.path.join(directory, filename2)
+
+with open(full_path, 'w') as file:
+    file.write('let analise = ' + json.dumps(json_data, indent=4) + ';')
---
+++ public/id.js
@@ -0,0 +1,602 @@
+let analise = {
+    "(1, 'Base')": {
+        "tempo_medio_execucao": 1.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(1, 'V86A')": {
+        "tempo_medio_execucao": 6.666666666666667,
+        "quantidade_execucoes": 3,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 3
+    },
+    "(1, 'V86B')": {
+        "tempo_medio_execucao": 1.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(1, 'V86C')": {
+        "tempo_medio_execucao": 1.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(1, 'V86D')": {
+        "tempo_medio_execucao": 1.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(2, 'Base')": {
+        "tempo_medio_execucao": 2.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(2, 'V86A')": {
+        "tempo_medio_execucao": 2000.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(2, 'V86B')": {
+        "tempo_medio_execucao": 21.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(2, 'V86C')": {
+        "tempo_medio_execucao": 2.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(2, 'V86D')": {
+        "tempo_medio_execucao": 2.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(3, 'Base')": {
+        "tempo_medio_execucao": 3.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(3, 'V86A')": {
+        "tempo_medio_execucao": 2003.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(3, 'V86B')": {
+        "tempo_medio_execucao": 13.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(3, 'V86C')": {
+        "tempo_medio_execucao": 3.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(3, 'V86D')": {
+        "tempo_medio_execucao": 3.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(4, 'Base')": {
+        "tempo_medio_execucao": 4.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(4, 'V86A')": {
+        "tempo_medio_execucao": 4.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(4, 'V86B')": {
+        "tempo_medio_execucao": 4.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(4, 'V86C')": {
+        "tempo_medio_execucao": 4.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(4, 'V86D')": {
+        "tempo_medio_execucao": 4.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(5, 'Base')": {
+        "tempo_medio_execucao": 5.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 1
+    },
+    "(5, 'V86A')": {
+        "tempo_medio_execucao": 16.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(5, 'V86B')": {
+        "tempo_medio_execucao": 5.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(5, 'V86C')": {
+        "tempo_medio_execucao": 5.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(5, 'V86D')": {
+        "tempo_medio_execucao": 5.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(6, 'Base')": {
+        "tempo_medio_execucao": 6.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(6, 'V86A')": {
+        "tempo_medio_execucao": 6.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(6, 'V86B')": {
+        "tempo_medio_execucao": 16.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(6, 'V86C')": {
+        "tempo_medio_execucao": 6.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(6, 'V86D')": {
+        "tempo_medio_execucao": 6.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(7, 'Base')": {
+        "tempo_medio_execucao": 7.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(7, 'V86A')": {
+        "tempo_medio_execucao": 7.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(7, 'V86B')": {
+        "tempo_medio_execucao": 7.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(7, 'V86C')": {
+        "tempo_medio_execucao": 7.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(7, 'V86D')": {
+        "tempo_medio_execucao": 7.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(8, 'Base')": {
+        "tempo_medio_execucao": 8.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(8, 'V86A')": {
+        "tempo_medio_execucao": 13.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(8, 'V86B')": {
+        "tempo_medio_execucao": 8.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(8, 'V86C')": {
+        "tempo_medio_execucao": 8.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(8, 'V86D')": {
+        "tempo_medio_execucao": 8.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(9, 'Base')": {
+        "tempo_medio_execucao": 9.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(9, 'V86A')": {
+        "tempo_medio_execucao": 9.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(9, 'V86B')": {
+        "tempo_medio_execucao": 9.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(9, 'V86C')": {
+        "tempo_medio_execucao": 9.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(9, 'V86D')": {
+        "tempo_medio_execucao": 9.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(10, 'Base')": {
+        "tempo_medio_execucao": 10.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(10, 'V86A')": {
+        "tempo_medio_execucao": 20.0,
+        "quantidade_execucoes": 2,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 1
+    },
+    "(10, 'V86B')": {
+        "tempo_medio_execucao": 10.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(10, 'V86C')": {
+        "tempo_medio_execucao": 10.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(10, 'V86D')": {
+        "tempo_medio_execucao": 10.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(11, 'Base')": {
+        "tempo_medio_execucao": 11.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(11, 'V86A')": {
+        "tempo_medio_execucao": 11.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(11, 'V86B')": {
+        "tempo_medio_execucao": 11.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(11, 'V86C')": {
+        "tempo_medio_execucao": 11.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 1
+    },
+    "(11, 'V86D')": {
+        "tempo_medio_execucao": 11.0,
+        "quantidade_execucoes": 2,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 1
+    },
+    "(12, 'Base')": {
+        "tempo_medio_execucao": 12.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 1
+    },
+    "(12, 'V86A')": {
+        "tempo_medio_execucao": 14.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(12, 'V86B')": {
+        "tempo_medio_execucao": 12.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(12, 'V86C')": {
+        "tempo_medio_execucao": 12.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(12, 'V86D')": {
+        "tempo_medio_execucao": 12.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(13, 'Base')": {
+        "tempo_medio_execucao": 13.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(13, 'V86A')": {
+        "tempo_medio_execucao": 23.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(13, 'V86B')": {
+        "tempo_medio_execucao": 13.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(13, 'V86C')": {
+        "tempo_medio_execucao": 13.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(13, 'V86D')": {
+        "tempo_medio_execucao": 13.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(14, 'Base')": {
+        "tempo_medio_execucao": 14.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(14, 'V86A')": {
+        "tempo_medio_execucao": 14.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(14, 'V86B')": {
+        "tempo_medio_execucao": 14.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(14, 'V86C')": {
+        "tempo_medio_execucao": 14.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(14, 'V86D')": {
+        "tempo_medio_execucao": 14.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(15, 'Base')": {
+        "tempo_medio_execucao": 15.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(15, 'V86A')": {
+        "tempo_medio_execucao": 15.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(15, 'V86B')": {
+        "tempo_medio_execucao": 15.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(15, 'V86C')": {
+        "tempo_medio_execucao": 15.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(15, 'V86D')": {
+        "tempo_medio_execucao": 15.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(16, 'Base')": {
+        "tempo_medio_execucao": 16.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 1
+    },
+    "(16, 'V86A')": {
+        "tempo_medio_execucao": 16.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(16, 'V86B')": {
+        "tempo_medio_execucao": 16.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(16, 'V86C')": {
+        "tempo_medio_execucao": 16.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(16, 'V86D')": {
+        "tempo_medio_execucao": 16.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(17, 'Base')": {
+        "tempo_medio_execucao": 17.0,
+        "quantidade_execucoes": 2,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 1
+    },
+    "(17, 'V86A')": {
+        "tempo_medio_execucao": 27.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(17, 'V86B')": {
+        "tempo_medio_execucao": 17.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(17, 'V86C')": {
+        "tempo_medio_execucao": 17.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(17, 'V86D')": {
+        "tempo_medio_execucao": 17.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(18, 'Base')": {
+        "tempo_medio_execucao": 18.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(18, 'V86A')": {
+        "tempo_medio_execucao": 18.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(18, 'V86B')": {
+        "tempo_medio_execucao": 18.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(18, 'V86C')": {
+        "tempo_medio_execucao": 18.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(18, 'V86D')": {
+        "tempo_medio_execucao": 18.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 1
+    },
+    "(19, 'Base')": {
+        "tempo_medio_execucao": 19.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(19, 'V86A')": {
+        "tempo_medio_execucao": 29.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(19, 'V86B')": {
+        "tempo_medio_execucao": 19.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(19, 'V86C')": {
+        "tempo_medio_execucao": 19.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(19, 'V86D')": {
+        "tempo_medio_execucao": 19.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(20, 'Base')": {
+        "tempo_medio_execucao": 20.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(20, 'V86A')": {
+        "tempo_medio_execucao": 20.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(20, 'V86B')": {
+        "tempo_medio_execucao": 20.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(20, 'V86C')": {
+        "tempo_medio_execucao": 20.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    },
+    "(20, 'V86D')": {
+        "tempo_medio_execucao": 20.0,
+        "quantidade_execucoes": 1,
+        "quantidade_executores": 1,
+        "quantidade_falhas_teste": 0
+    }
+};
