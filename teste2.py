e58016b1725342e66e4efd58904bce6ef0af722d
João Prado <58610872+joaoprado96@users.noreply.github.com>
2023-08-09 09:33:12 -0300

    Atualizações

---
+++ teste2.py
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Principal</title>
+</head>
+<body>
+    <button onclick="abrirJanela('caminho_do_seu_arquivo.txt')">Abrir Janela</button>
+
+    <script>
+        function abrirJanela(caminhoDoArquivo) {
+            const url = 'nova_janela.html?arquivo=' + encodeURIComponent(caminhoDoArquivo);
+            window.open(url, '_blank', 'width=500, height=400');
+        }
+    </script>
+</body>
+</html>
+
+
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nova Janela</title>
+</head>
+<body>
+    <pre id="conteudo"></pre> <!-- Usando a tag <pre> para manter a formatação do arquivo de texto -->
+
+    <script>
+        function getQueryParam(name) {
+            const urlParams = new URLSearchParams(window.location.search);
+            return urlParams.get(name);
+        }
+
+        function carregarArquivo() {
+            const caminhoDoArquivo = getQueryParam('arquivo');
+            fetch(caminhoDoArquivo)
+                .then(response => response.text())
+                .then(data => {
+                    document.getElementById('conteudo').textContent = data;
+                })
+                .catch(error => {
+                    console.error("Erro ao carregar o arquivo:", error);
+                });
+
+            // Atualizar a página a cada 2 segundos
+            setTimeout(function(){
+                location.reload();
+            }, 2000);
+        }
+
+        carregarArquivo();
+    </script>
+</body>
+</html>
