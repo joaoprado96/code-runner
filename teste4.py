daf36b44bbc63a354dd2385adafae1ad4f3fce6e
João Prado <58610872+joaoprado96@users.noreply.github.com>
2023-08-26 09:44:31 -0300

    Atualizar o teste4.py

--- teste4.py
+++ teste4.py
@@ -193,3 +193,118 @@ print(json_str)
 </html>
 
 
+1 Selection View Go Run Terminal
+Help
+CONTROL
+E VO ..
+«> snaprunner.html
+# snaprunner.css
+public > Is pages > 15 snaprunnerjs › ( analisar
+† incidentrunner.py
+e (Ctri+ Enter to commit on "ma...
+var dados;
+V Commit
+es
+unner.py front
+var JsonNavigator - document.getElementById("¡sonNavigator");
+var breadcrumb - document. getElementById("breadcrumb");
+var dataValues • document.getElementByld("dataValues");
+itau-mi-testes-coderunn
+us snaprunner.is ×
+ snaprunner.py 9+, M O
+ regressivo.py
+• 00A13
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+async function analisar() A
+// Obtenha o valor do usuário e da senha dos elementos de entrada
+const usuario
+•document.getElementById('usuario').value;
+const senha
+•document.getElementById('senha*).value:
+const pacote
+•document-getElementById(°pacote°).value;
+I
+const versao
+•document.getElementById(°versao°).value;
+const datasets - document-getElementById("datasets") . value;
+const id_snap - document.getElementById("¡d_snap") .value;
+alert ("Vamos processar sua solicitação, aguarde até aparecer as opções para navegar nas areas de memória, o tempo estimado é de 10 segundos")
+// Verifique se o usuário e a senha foram preenchidos
+if (Susuario II Isenha) {
+return; // Pare a execução da função
+const response2 - await fetch(*/front/snaprunner',
+method: 'POST',
+headers: {
+"Content-Type°: 'application/json°
+// Inclua o usuário e a senha no corpo da solicitação
+3);
+bodys JSON.stringify(fracf: usuario, senha: senha, pacote:pacote, versao: versao, datasets datasets, 16_snap: id_snap))
+dados - await response2.json();
+render0bject (dados)
+function render0bject (obj, breadcrumbPatt - [1) {
+dataValues.innerHIMLe
+jsonNavigatorinnerhTML
+ao.
+// Adiciona o botão de voltar, se estiver em um nível que não seja o raiz
+1f (breadcrumbPath. length › 0) {
+const backButton - document.createElement ("button");
+backButton.textContent-"Voltar"3
+backButton.onclick - () => {
+breadcrumbPath.pop();
+let obj - dados;
+breadcrumbPath =[];
+breadcrumb.innerHTML - mag
+renderobject (obj, breadcrumbPath);
+}8
+jsonNavigator.appendChild(backButton);
+PROBLEMS
+OUTPUT
+O CO
+TERMINAL
+POLYGLOT NOTEBOOK DEBUGCONSOLE
+[snaprunner] Code Runners O script snaprunner.py finalizou com Exit Coder 0 Bit Signal; num
+
+
