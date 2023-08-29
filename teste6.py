3652a142737d21f6f8f7082b1f2e833095c9ddee
João Prado <58610872+joaoprado96@users.noreply.github.com>
2023-08-29 10:12:10 -0300

    ATUALIZAZR

--- teste6.py
+++ teste6.py
@@ -2,34 +2,44 @@ import re
 import json
 
 def extract_fields(input_str):
-    # Defina a expressão regular para extrair os campos
-    regex = r"(\w+)\s*-\s*(\w+)\s*-\s*(\w+)\s*-\s*([\w\s'EQUX']+)\s*(\*.+\*)"
+    # Define the regular expression to extract the fields
+    regex = r"(\w+)\s*-\s*(\w+)\s*-\s*(\w+)\s*-\s*([\w\s'EQUX']+)(?:\s{3,}|(\*.*))"
     
-    # Use re.search para encontrar a primeira ocorrência que corresponde à expressão regular
+    # Use re.search to find the first occurrence that matches the regular expression
     match = re.search(regex, input_str)
     
     if match:
-        # Extrai os grupos correspondentes
+        # Extract the matching groups
         field1 = match.group(1).strip()
         field2 = match.group(2).strip()
         field3 = match.group(3).strip()
         field4 = match.group(4).strip()
-        field5 = match.group(5).strip()
+        field5 = match.group(5)
         
-        # Cria um dicionário com os campos extraídos
-        result ={
-            "MODULO": field2 +' (' +field1 +')',
-            "LABEL": field3,
-            "TRACE": field4,
-            "COMENTARIO": field5,
+        # Create a dictionary with the extracted fields
+        result = {
+            "Campo1": field1,
+            "Campo2": field2,
+            "Campo3": field3,
+            "Campo4": field4
         }
+
+        if field5:
+            result["Campo5"] = field5.strip()
         
-        # Retorna o dicionário como uma string JSON formatada
+        # Return the dictionary as a formatted JSON string
         return json.dumps(result, indent=4)
     else:
         return "Formato inválido"
 
-# Teste a função
-input_str = "D4 - MIAM - MIA00415 - TRC02     EQU    X'02'               * ANTES DA 2. CHAMADA NO MTRC   * asdasdasdas"
-json_result = extract_fields(input_str)
-print(json_result)
+# Test the function
+input_str1 = "D4 - MIAM - MIA00415 - TRC02     EQU    X'02'               * ANTES DA 2. CHAMADA NO MTRC   *"
+input_str2 = "D4 - MIAM - MIA00415 - TRC02     EQU    X'02'               * ANTES DA 2. CHAMADA NO MTRC   "
+
+json_result1 = extract_fields(input_str1)
+json_result2 = extract_fields(input_str2)
+
+print("Resultado 1:")
+print(json_result1)
+print("Resultado 2:")
+print(json_result2)
