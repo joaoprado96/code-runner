cb09868ca33d7583614a0dc59ba3471a1cb04461
João Prado <58610872+joaoprado96@users.noreply.github.com>
2023-08-31 15:54:19 -0300

    ASOFS

--- teste8.py
+++ teste8.py
@@ -1,62 +1,3 @@
-import json
-import re
-
-def process_lines(lines):
-    data = {}
-    current_line = ""
-    
-    for line in lines:
-        # Concatenate lines if the line is a continuation of the previous line
-        if line[-1].strip() == 'XMTTR' or line[-1].strip() == 'MTTR':
-            current_line += line.strip()[:71]  # Only consider content up to column 71
-        else:
-            current_line += ' ' + line.strip()[:71]
-            
-        # If this is the last part of the line, process it
-        if line[-1].strip() == 'MTTR':
-            transid = re.search(r'TRANSID=(\d+)', current_line)
-            transid1 = re.search(r'TRANSID1=(\d+)', current_line)
-            transid2 = re.search(r'TRANSID2=(\w+)', current_line)
-            
-            if transid:
-                transid = transid.group(1)
-                if transid not in data:
-                    data[transid] = {}
-                    
-                transid1 = transid1.group(1) if transid1 else '0'
-                transid2 = transid2.group(1) if transid2 else '0000'
-                
-                key = f"{transid1}{transid2}"
-                
-                if key not in data[transid]:
-                    data[transid][key] = {}
-                    
-                for attr in re.findall(r'(\w+)=\(([^)]+)\)|(\w+)=(\w+)', current_line):
-                    if attr[0]:
-                        data[transid][key][attr[0]] = attr[1].split(',')
-                    else:
-                        data[transid][key][attr[2]] = attr[3]
-            
-            current_line = ""
-            
-    return data
-
-# Test the function
-lines = [
-    'MTBTRA47 TRANSID=001,TRANSID1=2,TRANSID2=GF18,ATIVA=SIM,      XMTTR',
-    '               GRUPO=(G00,G01,G03,P002)     CONSITENCIA                 MTTR',
-    'MTBTRA47 TRANSID=001,TRANSID1=2,TRANSID2=GF19,ATIVA=SIM,      XMTTR',
-    '               GRUPO=(G00,G01,G03,P002),    CONSITENCIA                XMTTR',
-    '               TERM=(00,01,03,02),          CONSITENCIA                XMTTR',
-    '               PROGID=X0BH                                              MTTR',
-    'MTBTRA47 TRANSID=002,TRANSID1=2,TRANSID2=GF18,ATIVA=SIM        MTTR',
-    'MTBTRA47 TRANSID=011,ATIVA=SIM                                 MTTR',
-    # ... (rest of the lines)
-]
-
-data = process_lines(lines)
-print(json.dumps(data, indent=4))
-
 import re
 
 def remove_words_without_assignment(s):
@@ -67,11 +8,11 @@ def remove_words_without_assignment(s):
     words_with_assignment = re.findall(pattern, s)
     
     # Juntar palavras com atribuição de volta em uma string
-    result = ', '.join(words_with_assignment)
+    result = ' '.join(words_with_assignment)
     
     return result
 
 # Teste a função
-s = "INICIAL=JOAO, INICIAL=(JOAO,PRADO,SANTOS), CONSITENCIA, MTTR, NOME"
+s = "INICIAL=JOAO INICIAL=(JOAO,PRADO,SANTOS) CONSITENCIA MTTR NOME"
 new_s = remove_words_without_assignment(s)
 print(new_s)
