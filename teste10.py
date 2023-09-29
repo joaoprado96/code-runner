b2f1a09867a61c7f797d4079814f6ab8d04ec961
João Prado <58610872+joaoprado96@users.noreply.github.com>
2023-09-29 15:35:08 -0300

    Atualizar o teste10.py

--- teste10.py
+++ teste10.py
@@ -1,3 +1,25 @@
+
+
+from datetime import datetime, timedelta
+
+def generate_date_list(start_date_str, end_date_str):
+    start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
+    end_date = datetime.strptime(end_date_str, '%Y-%m-%d')
+
+    date_list = []
+
+    while start_date <= end_date:
+        date_list.append(start_date.strftime('%Y-%m-%d'))
+        start_date += timedelta(days=1)
+
+    return date_list
+
+# Exemplo de uso:
+start_date = '2023-09-10'
+end_date = '2023-09-15'
+print(generate_date_list(start_date, end_date))
+
+
 import json
 
 def filtrar_transacoes(data, filtro):
