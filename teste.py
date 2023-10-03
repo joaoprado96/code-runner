b73c0ea8e8c87828b5688742b498a19398170aa7
João Prado <58610872+joaoprado96@users.noreply.github.com>
2023-10-03 18:13:48 -0300

    Atualizar o teste.py

--- teste.py
+++ teste.py
@@ -4,14 +4,22 @@ import json
 # Lista para armazenar os conteúdos dos arquivos JSON
 json_list = []
 
+def remove_comments_from_json(json_string):
+    lines = json_string.splitlines()
+    cleaned_lines = [line for line in lines if not line.strip().startswith('//')]
+    return '\n'.join(cleaned_lines)
+
 # Lista todos os arquivos no diretório atual
 for filename in os.listdir('.'):
-    # Verifica se o arquivo tem a extensão .json
     if filename.endswith('.json'):
-        # Abre e lê o arquivo JSON
-        with open(filename, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-            json_list.append(data)
+        try:
+            with open(filename, 'r', encoding='utf-8') as file:
+                content_without_comments = remove_comments_from_json(file.read())
+                data = json.loads(content_without_comments)
+                json_list.append(data)
+        except json.decoder.JSONDecodeError:
+            print(f"Erro ao ler o arquivo {filename}!")
+            continue
 
 # Se você quiser salvar a lista em um novo arquivo JSON:
 with open('combined.json', 'w', encoding='utf-8') as outfile:
