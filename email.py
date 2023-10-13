3614a14b341379b37bf79a6e349fdc528475e197
João Prado <58610872+joaoprado96@users.noreply.github.com>
2023-10-13 15:00:45 -0300

    asdas

--- email.py
+++ email.py
@@ -1,38 +1,19 @@
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+from exchangelib import Credentials, Account, DELEGATE, Message, DELEGATE
 
-def send_outlook_email(subject, body, to_email, from_email, password):
-    # Configurações SMTP para Outlook
-    SMTP_SERVER = 'smtp-mail.outlook.com'
-    SMTP_PORT = 587  # TLS port
+# Defina suas credenciais
+credentials = Credentials('your_email@example.com', 'your_password')
 
-    # Criar mensagem
-    msg = MIMEMultipart()
-    msg['From'] = from_email
-    msg['To'] = to_email
-    msg['Subject'] = subject
-    msg.attach(MIMEText(body, 'plain'))
+# Crie uma conta e especifique o servidor (opcionalmente) e as credenciais
+account = Account('your_email@example.com', credentials=credentials, autodiscover=True, access_type=DELEGATE)
 
-    try:
-        # Conexão e envio
-        server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
-        server.starttls()
-        server.login(from_email, password)
-        server.sendmail(from_email, to_email, msg.as_string())
-        server.quit()
-
-        print("E-mail enviado com sucesso!")
-    except Exception as e:
-        print(f"Erro ao enviar e-mail: {e}")
-
-# Exemplo de uso
-if __name__ == "__main__":
-    your_email = 'seu_email@outlook.com'
-    your_password = 'sua_senha'
-    
-    recipient_email = 'destinatario@example.com'
-    subject = 'Teste de E-mail'
-    body = 'Olá! Isso é um teste de envio de e-mail via Outlook.'
-
-    send_outlook_email(subject, body, recipient_email, your_email, your_password)
+# Criar e enviar e-mail
+m = Message(
+    account=account,
+    folder=account.sent,
+    subject='Assunto do e-mail',
+    body='Corpo do e-mail',
+    to_recipients=['destinatario@example.com'],
+    cc_recipients=['cc@example.com'],  # opcional
+    bcc_recipients=['bcc@example.com']  # opcional
+)
+m.send_and_save()
