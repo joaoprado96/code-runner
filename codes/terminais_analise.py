f7889e52a4f9566aac350ba176848fae0bbfb34b
João Prado <58610872+joaoprado96@users.noreply.github.com>
2023-12-04 08:47:22 -0300

    Atualização geral

--- codes/terminais_analise.py
+++ codes/terminais_analise.py
@@ -7,43 +7,6 @@ from datetime import datetime, timedelta
 
 sql = MySQLHandler(host='localhost',user='root',password='12121212',database='coderunner')
 
-TABELA_APLICACAO = {
-    'identificador':'TEXT',
-    'agencia':'TEXT',
-    'instituicao':'TEXT',
-    'numeroserie':'TEXT',
-    'monitor':'TEXT',
-    'descricao':'TEXT',
-    'servidor':'TEXT',
-    'criador':'TEXT',
-    'tipo_terminal':'TEXT',
-    'data_criacao':'DATE',
-    'data_modificacao':'DATE',
-    'ultima_atualizacao_x0':'DATE'
-}
-
-TABELA_DISTRIBUIDA = {
-    'identificador':'TEXT',
-    'agencia':'TEXT',
-    'instituicao':'TEXT',
-    'numeroserie':'TEXT',
-    'monitor':'TEXT',
-    'descricao':'TEXT',
-    'servidor':'TEXT',
-    'criador':'TEXT',
-    'tipo_terminal':'TEXT',
-    'data_criacao':'DATE',
-    'data_modificacao':'DATE',
-    'em_uso':'TEXT'
-}
-
-
-# criar = sql.create_table(table_name='terminais_prod_aplicacao',json_structure=TABELA_APLICACAO)
-# print(criar)
-
-# criar = sql.create_table(table_name='terminais_prod_distribuida',json_structure=TABELA_DISTRIBUIDA)
-# print(criar)
-
 query       = '''
 SELECT 
     COALESCE(d.descricao, 'Disponíveis') AS descricao,
