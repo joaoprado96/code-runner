f12c24b2cbff806fa575bf8129a8c7ebd38b3aad
João Prado <58610872+joaoprado96@users.noreply.github.com>
2024-02-07 08:20:44 -0300

    Função de leitura de arquivos

--- coderunner/functions/jsonFunctions.js
+++ coderunner/functions/jsonFunctions.js
@@ -1,3 +1,22 @@
+const fs = require('fs').promises; // Importa a versão do fs que suporta Promises
+const path = require('path');
+
+async function loadJsonFileAsync(fileName) {
+  // Constrói o caminho absoluto para o arquivo dentro da pasta config
+  const absolutePath = path.join(__dirname, '../config', fileName);
+  
+  try {
+    // Lê o conteúdo do arquivo de forma assíncrona
+    const rawData = await fs.readFile(absolutePath, 'utf8');
+    // Transforma a string JSON em um objeto JavaScript
+    const jsonData = JSON.parse(rawData);
+    return jsonData;
+  } catch (error) {
+    console.error(`Erro ao ler o arquivo ${absolutePath}:`, error);
+    throw error; // Ou pode optar por retornar null ou um objeto vazio
+  }
+}
+
 function removeKeysFromObjects(objectList, keysToRemove) {
     // Verifica se objectList é realmente um array
     if (!Array.isArray(objectList)) {
@@ -48,6 +67,15 @@ function removeKeysFromObjects(objectList, keysToRemove) {
   
   module.exports = {
     removeKeysFromObjects,
-    addKeyValueToObjects
+    addKeyValueToObjects,
+    loadJsonFileAsync
   }
   
+
+  // Como usar:
+//   try {
+//     const configData = await loadJsonFileAsync('monitor.json');
+//     console.log(configData);
+//   } catch (error) {
+//     console.error('Não foi possível carregar o arquivo JSON', error);
+//   }
