8b39e8627464f980d8e168f97518b29ba2b1e1a6
João Prado <58610872+joaoprado96@users.noreply.github.com>
2024-02-09 02:34:29 -0300

    Atualizar o frint.ts

--- frint.ts
+++ frint.ts
@@ -1,9 +1,16 @@
-interface ApiResponse {
-  [key: string]: any; // Permitindo que qualquer chave seja um string e seu valor seja qualquer coisa.
-}
-
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import * as XLSX from 'xlsx';
+
+interface ApiResponse {
+  resultado: boolean;
+  resultado_comando: any[];
+  mensagem?: string;
+  monitor?: string;
+}
 
 @Component({
   selector: 'app-tabela-dinamica',
@@ -11,38 +18,41 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./tabela-dinamica.component.css']
 })
 export class TabelaDinamicaComponent implements OnInit {
+  dataSource = new MatTableDataSource<any>([]);
   colunasExibidas: string[] = [];
-  dataSource: ApiResponse[] = [];
 
-  constructor(private http: HttpClient) {}
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
+  constructor(private http: HttpClient, public dialog: MatDialog) {}
 
   ngOnInit() {
     this.carregarDados({ monitor: 'nomeDoMonitor', comando: 'comandoEspecifico' });
   }
 
   carregarDados(payload: { monitor: string; comando: string }): void {
-    this.http.post<ApiResponse[]>('https://exemplo.com/api/comandos', payload).subscribe(dados => {
-      this.dataSource = dados;
-      if (dados && dados.length > 0) {
-        this.colunasExibidas = Object.keys(dados[0]);
+    this.http.post<ApiResponse>('https://exemplo.com/api/comandos', payload).subscribe({
+      next: (resposta) => {
+        if (resposta.resultado && resposta.resultado_comando) {
+          this.dataSource.data = resposta.resultado_comando;
+          this.colunasExibidas = Object.keys(resposta.resultado_comando[0]);
+          this.dataSource.sort = this.sort;
+        } else {
+          this.mostrarPopupErro(resposta.mensagem, resposta.monitor);
+        }
+      },
+      error: (erro) => {
+        console.error('Erro ao buscar dados:', erro);
+        this.mostrarPopupErro("Erro de comunicação com a API.", "");
       }
-    }, error => {
-      console.error('Erro ao buscar dados:', error);
     });
   }
-}
 
-<table>
-  <thead>
-    <tr>
-      <th *ngFor="let coluna of colunasExibidas">{{ coluna | uppercase }}</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr *ngFor="let dado of dataSource">
-      <td *ngFor="let coluna of colunasExibidas">
-        {{ dado[coluna] }}
-      </td>
-    </tr>
-  </tbody>
-</table>
+  mostrarPopupErro(mensagem: string, monitor: string) {
+    this.dialog.open(DialogComponent, {
+      width: '250px',
+      data: {mensagem: mensagem, monitor: monitor}
+    });
+  }
+
+  // Métodos para filtragem, ordenação e exportação para Excel...
+}
